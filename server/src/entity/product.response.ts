da3d93d0bdcb3b5b212df5541737cbce0ba6a8c8
Myles Berueda <myles.berueda@codemithril.com>
2021-09-11 16:20:24 -0700

    refactor: cleaned it up a bit

--- server/src/entity/product.response.ts
+++ server/src/entity/product.response.ts
@@ -1,25 +1,24 @@
 /*
- * File: \src\entity\user.response.ts
+ * File: \src\entity\product.response.ts
  * Project: cm-ecommerce\cm-ecommerce-server
  * Created Date: Saturday September 11th 2021
  * Author: Myles Berueda
  * -----
- * Last Modified: Saturday September 11th 2021 1:22:51 pm
+ * Last Modified: Saturday September 11th 2021 4:15:46 pm
  * -----
  * Copyright (c) 2021 MylesWritesCode
  * -----
  * HISTORY
  */
 import { createUnionType } from "type-graphql";
-
-import { User } from "./user.entity";
+import { Product } from "./product.entity";
 import { GeneralError } from "./general.error";
 
-export const UserResponse = createUnionType({
-  name: "UserResponse",
-  types: () => [ User, GeneralError ],
+export const ProductResponse = createUnionType({
+  name: "ProductResponse",
+  types: () => [ Product, GeneralError ],
   resolveType: value => {
     if ("error" in value) { return GeneralError; }
-    if ("username" in value) { return User; }
+    if ("name" in value) { return Product; }
   }
 });
