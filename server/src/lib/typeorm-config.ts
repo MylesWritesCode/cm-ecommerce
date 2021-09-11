ac9b476ea4de759f37d1379fb572dd3242a10de3
Myles Berueda <myles.berueda@codemithril.com>
2021-09-10 23:38:06 -0700

    enabled error logging

--- server/src/lib/typeorm-config.ts
+++ server/src/lib/typeorm-config.ts
@@ -3,12 +3,17 @@
  * Project: cm-ecommerce\cm-ecommerce-server
  * Created Date: Friday September 3rd 2021
  * Author: Myles Berueda
+ * Note: For logging options, check the following resource.
+ *       
  * -----
  * Last Modified: Friday September 3rd 2021 1:38:15 pm
  * -----
  * Copyright (c) 2021 MylesWritesCode
  * -----
  * HISTORY
+ * 10/09/2021 - myles.berueda
+ * Added comments for logging info. Check the kanban board for more info.
+ * https://bit.ly/38W4nWT
  */
 import { ConnectionOptions } from "typeorm";
 const env = process.env;
@@ -21,7 +26,6 @@ export const ormConfig: ConnectionOptions = {
    "password": env.DB_PASS,
    "database": env.DB_NAME,
    "synchronize": true,
-   "logging": true,
    "entities": [ "src/entity/**/*.ts" ],
    "migrations": [ "src/migration/**/*.ts" ],
    "subscribers": [ "src/subscriber/**/*.ts" ],
@@ -30,5 +34,14 @@ export const ormConfig: ConnectionOptions = {
       "migrationsDir": "src/migration",
       "subscribersDir": "src/subscriber"
    },
-   "connectTimeoutMS": 10000
+   "connectTimeoutMS": 10000,
+   /**
+    * Logger options
+    * We can use the following logging options (or simply just true) to set
+    * the level of logging to the console that you want for the app. In this
+    * case, I'm opting to not output errors unless I want to via console.log
+    * "logging": [ "query", "error", "schema", "warn", "info", "log" ]
+    * https://github.com/typeorm/typeorm/blob/master/docs/logging.md
+    */
+   "logging": [ "query", "error", "schema", "warn", "info", "log" ], 
 };
