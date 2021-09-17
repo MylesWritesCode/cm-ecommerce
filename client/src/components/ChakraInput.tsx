58d22d69d6d4c365698f5a485d610880ae5d73e3
Myles Berueda <myles.berueda@codemithril.com>
2021-09-16 18:37:14 -1000

    added width prop to ChakraInput

---
+++ client/src/components/ChakraInput.tsx
@@ -0,0 +1,53 @@
+/*
+ * File: /src/components/ChakraInput.tsx
+ * Project: cm-ecommerce/cm-ecommerce-client
+ * Created Date: Thursday September 16th 2021
+ * Author: Myles Berueda
+ * -----
+ * Last Modified: Thursday September 16th 2021 3:48:19 pm
+ * -----
+ * Copyright (c) 2021 MylesWritesCode
+ * -----
+ * HISTORY
+ */
+import React from "react";
+import { Field } from "formik";
+import {
+  FormControl,
+  FormHelperText,
+  FormLabel,
+  Input,
+  FormErrorMessage,
+} from "@chakra-ui/react";
+
+interface ChakraInputProps {
+  label: string;
+  name: string;
+  type?: string;
+  placeholder?: string;
+  helperText?: string;
+  width?: string | Array<number>;
+}
+
+export const ChakraInput: React.FC<ChakraInputProps> = ({ ...props }) => {
+  const { label, name, helperText, width, ...rest } = props;
+  return (
+    <Field name={name}>
+      {({ field, form }) => (
+        <FormControl
+          id={name}
+          py={3}
+          width={ width ? width : [270]}
+          isInvalid={form.errors[name] && form.touched[name]}
+        >
+          <FormLabel htmlFor={name} fontSize="xs" mb={0}>
+            {label}
+          </FormLabel>
+          <Input id={name} size="sm" {...field} {...rest} />
+          <FormHelperText fontSize="xx-small">{helperText}</FormHelperText>
+          <FormErrorMessage>{form.errors[name]}</FormErrorMessage>
+        </FormControl>
+      )}
+    </Field>
+  );
+};
--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -2,13 +2,10 @@ import { Container } from "../components/Container";
 import {
   Button,
   Flex,
-  FormControl,
-  FormHelperText,
-  FormLabel,
-  Input,
 } from "@chakra-ui/react";
-import { Formik, Field, Form } from "formik";
+import { Formik, Form } from "formik";
 import React, { useState } from "react";
+import { ChakraInput } from "../components/ChakraInput";
 
 const Index: React.FC<{}> = ({}) => {
   const [isSubmitting, setIsSubmitting] = useState(false);
@@ -27,41 +24,18 @@ const Index: React.FC<{}> = ({}) => {
           }}
         >
           <Form>
-            <Field name="login">
-              {({ field, form }) => (
-                <FormControl id="login" isRequired py={3} width={[270]}>
-                  <FormLabel mb={0} fontSize="xs">
-                    Username or email
-                  </FormLabel>
-                  <Input
-                    {...field}
-                    type="login"
-                    placeholder="Enter a username or email address"
-                    size="sm"
-                  />
-                  <FormHelperText fontSize="xx-small">
-                    We'll never share your email with anyone.
-                  </FormHelperText>
-                </FormControl>
-              )}
-            </Field>
-
-            <Field name="password">
-              {({ field, form }) => (
-                <FormControl id="password" isRequired py={3} width={[270]}>
-                  <FormLabel mb={0} fontSize="xs">
-                    Password
-                  </FormLabel>
-                  <Input
-                    {...field}
-                    name="password"
-                    type="password"
-                    placeholder="Enter your password"
-                    size="sm"
-                  />
-                </FormControl>
-              )}
-            </Field>
+            <ChakraInput
+              name="login"
+              label="Login"
+              placeholder="Enter a username or password"
+              helperText="We'll never share your email with anyone."
+            />
+            <ChakraInput
+              name="password"
+              label="Password"
+              placeholder="Password"
+              type="password"
+            />
             <Flex>
               <Button
                 size="sm"
