4e808efb6e53a474d76d7a53cdc4fa59f51896a6
Myles Berueda <myles.berueda@codemithril.com>
2021-09-16 15:15:46 -1000

    changed styles to width array for responsiveness

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -15,7 +15,7 @@ const Index: React.FC<{}> = ({}) => {
   return (
     // <Container height="100vh">
     <Container>
-      <Flex my={5} minWidth="768px" justifyContent="center" alignItems="center">
+      <Flex my={5} width={[768]} justifyContent="center" alignItems="center">
         <Formik
           initialValues={{
             login: "",
@@ -29,7 +29,7 @@ const Index: React.FC<{}> = ({}) => {
           <Form>
             <Field name="login">
               {({ field, form }) => (
-                <FormControl id="login" isRequired py={3} minWidth="270px">
+                <FormControl id="login" isRequired py={3} width={[270]}>
                   <FormLabel mb={0} fontSize="xs">
                     Username or email
                   </FormLabel>
@@ -48,7 +48,7 @@ const Index: React.FC<{}> = ({}) => {
 
             <Field name="password">
               {({ field, form }) => (
-                <FormControl id="password" isRequired py={3} minWidth="270px">
+                <FormControl id="password" isRequired py={3} width={[270]}>
                   <FormLabel mb={0} fontSize="xs">
                     Password
                   </FormLabel>
