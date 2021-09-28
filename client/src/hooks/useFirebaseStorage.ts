b3aae5043cc0bfc759f9346b94470da53cb0211f
Myles Berueda <myles.berueda@codemithril.com>
2021-09-27 15:31:22 -1000

    setting up firebase

---
+++ client/src/hooks/useFirebaseStorage.ts
@@ -0,0 +1,14 @@
+/*
+ * File: /src/hooks/useFirebaseStorage.ts
+ * Project: cm-ecommerce/cm-ecommerce-client
+ * Created Date: Monday September 27th 2021
+ * Author: Myles Berueda
+ * -----
+ * Last Modified: Monday September 27th 2021 3:26:14 pm
+ * -----
+ * Copyright (c) 2021 MylesWritesCode
+ * -----
+ * HISTORY
+ */
+import { useState, useEffect } from "react";
+import { firebaseStorage } from "@lib/firebase";
---
+++ client/src/lib/firebase.ts
@@ -0,0 +1,41 @@
+/*
+ * File: /src/lib/firebase.ts
+ * Project: cm-ecommerce/cm-ecommerce-client
+ * Created Date: Monday September 27th 2021
+ * Author: Myles Berueda
+ * -----
+ * Last Modified: Monday September 27th 2021 2:20:58 pm
+ * -----
+ * Copyright (c) 2021 MylesWritesCode
+ * -----
+ * HISTORY
+ */
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import { getStorage } from 'firebase/storage';
+import { getFirestore } from "firebase/firestore";
+
+const env = process.env;
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: env.FIREBASE_API_KEY,
+  authDomain: env.FIREBASE_AUTH_DOMAIN,
+  projectId: env.FIREBASE_PROJECT_ID,
+  storageBucket: env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: env.FIREBASE_APP_ID,
+  measurementId: env.FIREBASE_MEASUREMENT_ID
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const firebaseStorage = getStorage(app);
+const firebaseFirestore = getFirestore(app); // Probably not using this
+const analytics = getAnalytics(app);
+
+export { firebaseStorage, firebaseFirestore, };
