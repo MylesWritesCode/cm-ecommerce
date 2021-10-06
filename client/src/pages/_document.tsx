b2a714653c62fc64945bd8a32ca5fdbbe6cb44ce
Myles Berueda <myles.berueda@codemithril.com>
2021-10-06 10:31:19 -1000

    fix

--- client/src/pages/_document.tsx
+++ client/src/pages/_document.tsx
@@ -6,7 +6,7 @@ export default class Document extends NextDocument {
     return (
       <Html>
         <Head>
-          <link rel="shortcut icon" href="favicon.png" />
+          <link rel="shortcut icon" href="/favicon.png" />
         </Head>
         <body>
           {/* Make Color mode to persists when you refresh the page. */}
