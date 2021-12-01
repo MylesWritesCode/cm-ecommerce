73ead43cc86269683433199488b7879cec3a82b4
Myles Berueda <myles.berueda@codemithril.com>
2021-11-30 15:40:15 -1000

    scaffolding

---
+++ client/src/components/dashboard/Module.tsx
@@ -0,0 +1,15 @@
+/**
+ * File: /src/components/dashboard/Module.tsx
+ * Project: cm-ecommerce-client
+ * Purpose: This will be a module for the dashboard. It should be display data
+ *          from the db - such as sales totals, invoices, etc.
+ * 
+ * @author Myles Berueda
+ * @date   Tuesday November 30th 2021
+ * -----
+ * Modified: Tuesday November 30th 2021 3:39:10 pm
+ * -----
+ * Copyright (c) 2021 MylesWritesCode
+ * -----
+ * HISTORY
+**/
---
+++ client/src/pages/dashboard/index.tsx
@@ -0,0 +1,14 @@
+/**
+ * File: /src/pages/dashboard/index.tsx
+ * Project: cm-ecommerce-client
+ * Purpose: Primary dashboard for sellers
+ * 
+ * @author Myles Berueda
+ * @date   Tuesday November 30th 2021
+ * -----
+ * Modified: Tuesday November 30th 2021 3:37:54 pm
+ * -----
+ * Copyright (c) 2021 MylesWritesCode
+ * -----
+ * HISTORY
+**/
